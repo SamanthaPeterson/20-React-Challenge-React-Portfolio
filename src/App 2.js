1be7f56234a59e3bb63e8921f19c581e58a3948f
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-02 16:55:06 -0600

    update ap and index

--- src/App 2.js
+++ src/App 2.js
@@ -1,8 +1,69 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, {
+  useState
+} from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 
 function App() {
-  return <Counter />;
+  const [categories] = useState([{
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    {
+      name: 'portraits',
+      description: 'Portraits of people in my life'
+    },
+    {
+      name: 'food',
+      description: 'Delicious delicacies'
+    },
+    {
+      name: 'landscape',
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  return ( <
+    div >
+    <
+    Nav categories = {
+      categories
+    }
+    setCurrentCategory = {
+      setCurrentCategory
+    }
+    currentCategory = {
+      currentCategory
+    }
+    contactSelected = {
+      contactSelected
+    }
+    setContactSelected = {
+      setContactSelected
+    } >
+    < /Nav> <
+    main > {
+      !contactSelected ? ( <
+        >
+        <
+        Portfolio currentCategory = {
+          currentCategory
+        } > < /Gallery> <
+        About > < /About> <
+        />
+      ) : ( <
+        ContactForm > < /ContactForm>
+      )
+    } <
+    /main> <
+    /div>
+  );
 }
 
 export default App;
