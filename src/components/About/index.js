f3ed4dac5c2f47c188f2533368a1e7259fda392b
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-06 16:10:42 -0600

    error with cover image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,18 +7,21 @@ function About() {
             <h2 class="top-title">Samantha Peterson</h2>
             <hr></hr>
             <div>
-                <img class="mb-5" id="avatar" src="https://avatars3.githubusercontent.com/u/11549316?s=460&u=9cbb3efedf0845c6fb20524c104ba92fff38c871&v=4" alt="Samantha Peterson" />
+                < img class = "mb-5"
+                id = "avatar"
+                src = "https://github.com/SamanthaPeterson" alt = "Samantha Peterson" />
 
                 <p>
-                Seasoned programmer with over 20 years of experience incorporating user and business requirements into cost-effective, 
-                secure and user-friendly solutions known for scalability and durability. 
+                Web developer coding in JavaScript.Enjoys Node JS and Express JS frameworks, as well as Figma
+                for web design.An Adobe Creative Cloud guru.Graphic designer, able to create beautiful imagery that communicates clearly.
                 </p>
                 <p>
-                Strong knowledge of commercial and open source CMS development 
-                software/database engineering tools.
+                Web developer coding in JavaScript.Enjoys Node JS and Express JS frameworks, as well as Figma
+                for web design.An Adobe Creative Cloud guru.Graphic designer, able to create beautiful imagery that communicates clearly.
                 </p>
                 <p>
-                Proven leader and consultant; drive system architecture decisions and lead projects from concept through the release process.
+                Web developer coding in JavaScript.Enjoys Node JS and Express JS frameworks, as well as Figma
+                for web design.An Adobe Creative Cloud guru.Graphic designer, able to create beautiful imagery that communicates clearly.
                 </p>
             </div>
         </section>
